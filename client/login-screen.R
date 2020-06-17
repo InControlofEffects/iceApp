e42e70f673032794d3f070ce6a3eac1d01bc489f
davidruvolo51 <davidruvolo51@gmail.com>
2020-06-17 18:07:39 +0200

    redesigned login screen

--- client/login-screen.R
+++ client/login-screen.R
@@ -2,65 +2,86 @@
 #' FILE: login-screen.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2019-08-27
-#' MODIFIED: 2020-06-15
+#' MODIFIED: 2020-06-17
 #' PURPOSE: login screen for shiny app
 #' PACKAGES: see global R
 #' COMMENTS: NA
 #'//////////////////////////////////////////////////////////////////////////////
 # build page
 loginScreen <- function() {
-    tags$div(class = "signin-screen",
-
-        # logo
-        tags$a(
-            class = "brandlink",
-            href = "https://incontrolofeffects.com",
-            # tags$img(class = "logo", src = "images/Logo.svg"),
-            appLogo(),
-            tags$span("In Control of Effects")
-        ),
-
-        # form
-        tags$form(id = "loginForm", class = "form",
-
-            # content
-            tags$legend("Welcome, please sign in."),
-
-            # error message if access credentials are incorrect
-            tags$span(id = "error-form", class = "error-text", role = "alert"),
-
-            # username
-            tags$label(`for` = "username", "Username"),
-            tags$input(
-                type = "text",
-                id = "username",
-                class = "form-control shiny-bound-input"
+    tags$div(
+        id = "signin-page",
+        class = "full-width-page filled-page-background",
+        tags$div(
+            class = "inner-page",
+            tags$form(
+                id = "signin-form",
+                class = "form",
+                rheroicons::solid$user_circle(aria_hidden = TRUE),
+                tags$h2(
+                    id = "signin-form-title",
+                    "Welcome, please sign in"
+                ),
+                tags$div(
+                    class = "error-box browsertools-hidden",
+                    id = "signin-form-error",
+                    role = "alert",
+                    `aria-hidden` = "true",
+                    rheroicons::outline$exclamation(aria_hidden = TRUE),
+                    tags$span(
+                        id = "signin-form-error-message",
+                        class = "error-box-text"
+                    )
+                ),
+                tags$label(`for` = "username", "Username"),
+                tags$input(
+                    type = "text",
+                    id = "username",
+                    class = "form-control shiny-bound-input"
+                ),
+                tags$div(
+                    class = "error-box browsertools-hidden",
+                    id = "signin-form-username-error",
+                    role = "alert",
+                    `aria-hidden` = "true",
+                    rheroicons::outline$exclamation(aria_hidden = TRUE),
+                    tags$span(
+                        id = "signin-form-username-error-message",
+                        class = "error-box-text"
+                    )
+                ),
+                tags$label(`for` = "password", "Password"),
+                tags$input(
+                    type = "password",
+                    id = "password",
+                    class = "form-control shiny-bound-input"
+                ),
+                tags$div(
+                    class = "error-box browsertools-hidden",
+                    id = "signin-form-password-error",
+                    role = "alert",
+                    `aria-hidden` = "true",
+                    rheroicons::outline$exclamation(aria_hidden = TRUE),
+                    tags$span(
+                        id = "signin-form-password-error-message",
+                        class = "error-box-text"
+                    )
+                ),
+                tags$button(
+                    id = "login",
+                    class = "action-button shiny-bound-input primary",
+                    type = "submit",
+                    "Sign In"
+                )
             ),
-            tags$span(
-                id = "error-username",
-                class = "error-text",
-                role = "alert"
-            ),
-
-            # password
-            tags$label(`for` = "password", "Password"),
-            tags$input(
-                type = "password",
-                id = "password",
-                class = "form-control shiny-bound-input"
-            ),
-            tags$span(
-                id = "error-password",
-                class = "error-text",
-                role = "alert"
-            ),
-
-            # buttons
-            tags$button(
-                id = "login",
-                class = "action-button shiny-bound-input primary",
-                type = "submit",
-                "Sign In"
+            tags$p(
+                id = "return-link",
+                "Return to the ",
+                tags$a(
+                    href = "#",
+                    "main"
+                ),
+                "site."
             )
         )
     )
