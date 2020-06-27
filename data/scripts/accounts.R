1dfa648e8674181e1e562521aa32498495565666
davidruvolo51 <davidruvolo51@gmail.com>
2020-06-27 18:14:41 +0200

    created sample user accounts

--- data/scripts/accounts.R
+++ data/scripts/accounts.R
@@ -1,8 +1,9 @@
 ## code to prepare `accounts` dataset goes here
 
 accounts <- data.frame(
-    username = c("admin", "standard"),
-    password = c("admin12345", "standard12345")
+    username = c("admin", "standard", "test"),
+    password = c("admin12345", "standard12345", "test"),
+    type = c("admin", "standard", "test")
 )
 accounts$password <- sapply(accounts$password, sodium::password_store)
 
