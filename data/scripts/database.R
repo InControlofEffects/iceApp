f3ca57d5e466f7af5db3651065c7514140995c99
davidruvolo51 <davidruvolo51@gmail.com>
2020-06-27 18:15:23 +0200

    initialized database and db class

---
+++ data/scripts/database.R
@@ -0,0 +1,129 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: database.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2020-06-27
+#' MODIFIED: 2020-06-27
+#' PURPOSE: all database things
+#' STATUS: in.progress
+#' PACKAGES: DBI; RSQLite
+#' COMMENTS: See utils analytics.R for corresponding function
+#'////////////////////////////////////////////////////////////////////////////
+
+# create db
+db <- DBI::dbConnect(drv = RSQLite::SQLite(), dbname = "data/ice.db")
+
+
+#'//////////////////////////////////////
+
+#' ~ 999 ~
+#' Create Side Effects Definitions
+#' Store the Side Effect IDs to Common Name
+side_effect_codes <- data.frame(
+    effect_name = c(
+        "akathisia",
+        "anticholinergic",
+        "antiparkinson",
+        "prolactic",
+        "qtc",
+        "sedation",
+        "weight_gain"
+    ),
+    public_name = c(
+        "Dry Mouth and Constipation",
+        "Feeling Sleepy or Drowsy",
+        "Irregular Heartbeat",
+        "Restlessness",
+        "Sexual Dysfunction",
+        "Stiffness and Tremor",
+        "Weight Gain"
+    )
+)
+
+# create
+DBI::dbCreateTable(conn = db, name = "definitions", fields = side_effect_codes)
+DBI::dbAppendTable(conn = db, name = "definitions", value = side_effect_codes)
+
+#' test
+#' DBI::dbFetch(DBI::dbSendQuery(db, "SELECT * FROM definitions"))
+
+
+#'//////////////////////////////////////
+
+#' ~ 0 ~
+#' Create Sessions
+#' The sessions table is used for storing records of all user sessions. Entries
+#' are dependent on the account data (user type) and application mode.
+#' @param id a unique randomized string for the session (see analytics.R)
+#' @param mode the application mode (i.e, dev, prod, testing, etc)
+#' @param time the time the session started (in relation to the server, !user)
+#' @param user_type set in accounts.R, admin, test, dev, etc...
+#' @param attempt a counter indicating the n trials the user has done
+sessions <- data.frame(
+    id = NA,
+    mode = NA,
+    time = NA,
+    user_type = NA,
+    attempt = NA
+)
+
+# create
+DBI::dbCreateTable(conn = db, name = "sessions", fields = sessions)
+
+#' Test
+#' DBI::dbFetch(DBI::dbSendQuery(db, "Select * from sessions"))
+
+#'//////////////////////////////////////
+
+#' ~ 1 ~
+#' Create Actions
+#' The actions table is used for logging app analytics such as button clicks
+#' and errors. Use R6 methods `capture_click` and `capture_error` for
+#' standarizing data entry.
+#' @param id session ID
+#' @param attempt a counter indicating the n trials the user has done
+#' @param time time of event
+#' @param event event type (e.g., click, error)
+#' @param event_id an ID for the event (e.g., "login_form")
+#' @param event_desc a description for the event (e.g., "missing username")
+actions <- data.frame(
+    id = NA,
+    attempt = NA,
+    time = NA,
+    event = NA,
+    event_id = NA,
+    event_desc = NA
+)
+
+# create
+DBI::dbCreateTable(conn = db, name = "actions", fields = actions)
+
+
+#'//////////////////////////////////////
+
+#' ~ 2 ~
+#' User Selections
+#' This table is used to save all user selections across attemps. This will be
+#' useful for validation of the algorithm and for future research.
+#' @param id session ID
+#' @param attempt current user attempt
+#' @param time time selections were submitted
+#' @param ... binary variables indicating if a side effect was selected (1:yes)
+selections <- data.frame(
+    id = NA,
+    attempt = NA,
+    time = NA,
+    akathisia = NA,
+    anticholinergic = NA,
+    antiparkinson = NA,
+    prolactic = NA,
+    qtc = NA,
+    sedation = NA,
+    weight_gain = NA
+)
+
+# create
+DBI::dbCreateTable(conn = db, name = "selections", fields = selections)
+
+
+# CHECK
+DBI::dbListTables(db)
--- utils/analytics.R
+++ utils/analytics.R
@@ -11,38 +11,143 @@
 #'      use `myObject$log_action()` to send data to the database
 #'////////////////////////////////////////////////////////////////////////////
 analytics <- R6::R6Class(
-    "analytics",
-    # public
+    "ice_analytics",
     public = list(
-        session_id = NA,
-        initialize = function() {
-            self$session_id <- private$new_id()
 
-            # log init to DB
-            self$log_action(
-                action = "session",
-                value = "initialized",
-                result = NA
+        # $new()
+        initialize = function(mode = "dev", user_type = "user") {
+
+            # define props
+            private$.id <- private$.new_id()
+            private$.mode <- mode
+            private$.time <- Sys.time()
+            private$.user_type <- user_type
+
+            # connect to database
+            private$.db_connect()
+
+            # write session info
+            private$.write_session_info()
+        },
+
+        # log button clicks
+        capture_click = function(btn, desc) {
+            private$.write_action(
+                event = "click",
+                event_id = btn,
+                event_desc = desc
             )
         },
-        log_action = function(action, value, result) {
-            d <- data.frame(
-                id = self$session_id,
-                time = Sys.time(),
-                action = action,
-                value = value,
-                result = result
-            )
 
-            # log action to DB
-            return(d)
+        # log errors
+        capture_error = function(error, message) {
+            private$.write_action(
+                event = "error",
+                event_id = error,
+                event_desc = message
+            )
+        },
+
+        # log user selections
+        capture_selections = function(selections) {
+            private$.write_selections(data = selections)
+        },
+
+        # update session attempts
+        update_attempts = function() {
+            private$.attempts <- private$.attempts + 1
+            private$.write_session_info()
+        },
+
+        # disconnect
+        disconnect = function() {
+            private$.db_disconnect()
         }
     ),
-    # private methods
     private = list(
-        new_id = function(length = 32) {
+
+        # set props
+        .db_name = "data/ice.db",
+        .db_conn = NA,
+        .id = NA,
+        .mode = NA,
+        .time = NA,
+        .user_type = NA,
+        .attempts = 1,
+
+        # connect to database
+        .db_connect = function() {
+            private$.db_conn <- DBI::dbConnect(
+                drv = RSQLite::SQLite(),
+                dbname = private$.db_name
+            )
+        },
+
+        # disconnect
+        .db_disconnect = function() {
+            DBI::dbDisconnect(private$.db_conn)
+        },
+
+        # generate new ID
+        .new_id = function(length = 32) {
             d <- sample(x = c(0:9, letters), size = length, replace = TRUE)
             return(paste0(d, collapse = ""))
+        },
+
+        # on Initial creation
+        .write_session_info = function() {
+            d <- data.frame(
+                id = private$.id,
+                mode = private$.mode,
+                time = private$.time,
+                user_type = private$.user_type,
+                attempt = private$.attempts
+            )
+            DBI::dbAppendTable(private$.db_conn, name = "sessions", value = d)
+        },
+
+        # write action
+        .write_action = function(event, event_id, event_desc) {
+
+            # define object data to write into table
+            d <- data.frame(
+                id = private$.id,
+                attempt = private$.attempts,
+                time = Sys.time(),
+                event = event,
+                event_id = event_id,
+                event_desc = event_desc
+            )
+
+            # append table
+            DBI::dbAppendTable(private$.db_conn, name = "actions", value = d)
+        },
+
+        # write user selections
+        .write_selections = function(data) {
+
+            # bind private data to data
+            d <- data
+            d$id <- private$.id
+            d$attempt <- private$.attempts
+            d$time <- Sys.time()
+
+            # reorder
+            d <- d[, c(
+                "id", "attempt", "time", "akathisia",
+                "anticholinergic", "antiparkinson",
+                "prolactic", "qtc", "sedation", "weight_gain"
+            )]
+
+            # write
+            DBI::dbAppendTable(
+                private$.db_conn,
+                name = "selections",
+                value = d
+            )
         }
     )
 )
+
+#' Tests
+client <- analytics$new()
